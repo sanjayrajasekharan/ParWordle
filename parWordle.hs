55f9914666ad17de9ac262e4c507e258645f988b
Sanjay Rajasekharan <sanjay.rajasekharan@gmail.com>
2022-12-17 15:14:06 -0500

    basic word lookup code

--- parWordle.hs
+++ parWordle.hs
@@ -1,8 +1,8 @@
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 import qualified Data.ByteString.Char8 as B
 import qualified System.Environment as Env
 import qualified System.Exit as Exit
-
 
 main :: IO ()
 main = do
@@ -13,4 +13,19 @@ main = do
    else do
       let file_name:_ = a
       f <- B.readFile file_name
-      mapM_ print $ filter (\x -> B.length x == 5) (B.words f)
+      print (Map.findWithDefault Set.empty 'a' ((buildMaps . getValidWords . B.words) f !! 4))
+
+getValidWords:: [B.ByteString] -> [B.ByteString]
+getValidWords = filter (\x -> B.length x == 5)
+
+buildMap:: [Char] -> [B.ByteString] -> Map.Map Char (Set.Set B.ByteString)
+buildMap k x = Map.fromListWith Set.union l
+    where l = zip k (map Set.singleton x)
+
+buildMapsHelper:: [B.ByteString] -> [B.ByteString] ->[Map.Map Char (Set.Set B.ByteString)]
+buildMapsHelper k x
+    | B.length (head k) == 0  = []
+    | otherwise = buildMap (map B.head k) x : buildMapsHelper (map B.tail k) x
+
+buildMaps:: [B.ByteString] ->[Map.Map Char (Set.Set B.ByteString)]
+buildMaps x = buildMapsHelper x x
